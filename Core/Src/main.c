f3190482c9a6fc678df157daf0685f163cef8a65
super-cow-powers <dm1306@york.ac.uk>
2022-10-20 12:56:20 +0100

    Add basic fade effect

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -5,16 +5,24 @@
 #include "fx.h"
 #include <stdint.h>
 
-uint8_t INIT_DATA_BUFFER[PIXEL_NUMBER][3] = {
+volatile uint8_t press = 0;
+
+const uint8_t INIT_DATA_BUFFER[PIXEL_NUMBER][3] = {
   {15, 50, 0}, {15, 50, 0}, {15, 50, 0}, {15, 50, 0}, {15, 50, 0},
   {15, 50, 0}, {15, 50, 0}, {15, 50, 0}, {15, 50, 0}, {15, 50, 0}};
-uint8_t ALT_DATA_BUFFER[PIXEL_NUMBER][3] = {
-  {15, 50, 100}, {50, 50, 0}, {100, 0, 10}, {20, 150, 75}, {200, 50, 10},
-  {15, 125, 5}, {50, 0, 100}, {15, 125, 25}, {5, 25, 150},{100, 10, 75}};
-uint8_t (*ACTIVE_DATA_BUFFER)[][3] = &INIT_DATA_BUFFER;
+const uint8_t ALT_DATA_BUFFER[PIXEL_NUMBER][3] = {
+  {15, 50, 100}, {50, 50, 0},  {100, 0, 10},  {20, 150, 75}, {200, 50, 10},
+  {15, 125, 5},  {50, 0, 100}, {15, 125, 25}, {5, 25, 150},  {100, 10, 75}};
+uint8_t FX_DATA_BUFFER[PIXEL_NUMBER][3] = {0};
+const uint8_t (*ACTIVE_DATA_BUFFER)[][3] = &INIT_DATA_BUFFER;
+
+#define FX_DELAY_mS 20
+#define LED_MAX 200
 
 int main(void)
 {
+  uint32_t lastFxRun = 0;
+  int8_t effectDir = 1;
   InitSys();
   clearData(PIXEL_NUMBER);
   for (uint8_t i = 0; i <= PIXEL_NUMBER; i++) {
@@ -22,21 +30,36 @@ int main(void)
     while (getTicks() < initTick+300);
     writeData(*ACTIVE_DATA_BUFFER, i);
   }
-  //writeData(*ACTIVE_DATA_BUFFER, (uint32_t)PIXEL_NUMBER);
+
   while(1){
-    if(getButtonPressed()){
-      if (ACTIVE_DATA_BUFFER == &INIT_DATA_BUFFER){
-	ACTIVE_DATA_BUFFER = &ALT_DATA_BUFFER;
-      } else {
-	ACTIVE_DATA_BUFFER = &INIT_DATA_BUFFER;
+    switch (press) {
+    case 0:
+      ACTIVE_DATA_BUFFER = &INIT_DATA_BUFFER;
+      break;
+    case 1:
+      ACTIVE_DATA_BUFFER = &ALT_DATA_BUFFER;
+      break;
+    case 3: {
+      ACTIVE_DATA_BUFFER = &FX_DATA_BUFFER;
+      if (getTicks() > lastFxRun + FX_DELAY_mS){
+	if (FX_DATA_BUFFER[0][0] >= LED_MAX - effectDir) {
+	  effectDir = -effectDir; //Go Down
+	} else if (FX_DATA_BUFFER[0][0] <=  -effectDir){
+	  effectDir = -effectDir; //Go Up
+	}
+	for (uint8_t i = 0; i < PIXEL_NUMBER; i++){
+	  FX_DATA_BUFFER[i][i%3] += effectDir;
+	}
+	lastFxRun = getTicks();
       }
+    }
+      break;
+    }
+    writeData(*ACTIVE_DATA_BUFFER, PIXEL_NUMBER);
+    if(getButtonPressed()){
+      press = (press+1)%4;
       clearButtonPress();
       clearData(PIXEL_NUMBER);
-      for (uint8_t i = 0; i <= PIXEL_NUMBER; i++) {
-	uint32_t initTick = getTicks();
-	while (getTicks() < initTick+300);
-	writeData(*ACTIVE_DATA_BUFFER, i);
-      }
     }
   }
 }
