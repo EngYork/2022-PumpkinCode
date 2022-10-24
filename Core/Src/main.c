d913e6ec5726772577384b99823c6da5cb723b33
super-cow-powers <dm1306@york.ac.uk>
2022-10-24 16:21:42 +0100

    Add a slightly rubbish flame effect

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -4,20 +4,37 @@
 #include "stm32l0xx_it.h"
 #include "fx.h"
 #include <stdint.h>
+#include <string.h>
 
 volatile uint8_t press = 0;
 
+//Initial static data buffer
 const uint8_t INIT_DATA_BUFFER[PIXEL_NUMBER][3] = {
   {15, 50, 0}, {15, 50, 0}, {15, 50, 0}, {15, 50, 0}, {15, 50, 0},
   {15, 50, 0}, {15, 50, 0}, {15, 50, 0}, {15, 50, 0}, {15, 50, 0}};
+//Secondary static data buffer
 const uint8_t ALT_DATA_BUFFER[PIXEL_NUMBER][3] = {
   {15, 50, 100}, {50, 50, 0},  {100, 0, 10},  {20, 150, 75}, {200, 50, 10},
   {15, 125, 5},  {50, 0, 100}, {15, 125, 25}, {5, 25, 150},  {100, 10, 75}};
+//Modifyable data buffer for effects.
 uint8_t FX_DATA_BUFFER[PIXEL_NUMBER][3] = {0};
 const uint8_t (*ACTIVE_DATA_BUFFER)[][3] = &INIT_DATA_BUFFER;
 
 #define FX_DELAY_mS 20
 #define LED_MAX 200
+
+//Base indexes for each "Column" of LEDs.
+const uint8_t COL_BASES[4] = {2, 5, 8, 9};
+//Size of each LED column. Reverse indexed from the "base".
+const uint8_t COL_SIZES[4] = {3,3,3,1};
+
+//Clear a data buffer
+void resetBuffer(uint8_t buffer[PIXEL_NUMBER][3]);
+// Generate a (not very) random number with an LFSR
+uint32_t l_rand(void);
+//Init LFSR Seed
+uint32_t LFSR = 0xDEADBEEF;
+uint32_t LFSR_BIT = 0;
 
 int main(void)
 {
@@ -39,9 +56,9 @@ int main(void)
     case 1:
       ACTIVE_DATA_BUFFER = &ALT_DATA_BUFFER;
       break;
-    case 3: {
+    case 2: { //Fade in fade out
       ACTIVE_DATA_BUFFER = &FX_DATA_BUFFER;
-      if (getTicks() > lastFxRun + FX_DELAY_mS){
+      if (getTicks() > lastFxRun + 20){
 	if (FX_DATA_BUFFER[0][0] >= LED_MAX - effectDir) {
 	  effectDir = -effectDir; //Go Down
 	} else if (FX_DATA_BUFFER[0][0] <=  -effectDir){
@@ -54,12 +71,52 @@ int main(void)
       }
     }
       break;
+    case 3: { //Cheapo flame effect
+      ACTIVE_DATA_BUFFER = &FX_DATA_BUFFER;
+      if (getTicks() > lastFxRun + 250){
+	resetBuffer(FX_DATA_BUFFER);
+	for (uint8_t i = 0; i < 4; i++) {
+	  uint8_t RAN = (uint8_t)l_rand();
+	  uint8_t leds_on; //Number of LEDS on
+	  if (COL_SIZES[i] == 1){
+	    leds_on = RAN % 2;
+	  } else {
+	    leds_on = (RAN % COL_SIZES[i]) + 1;
+	  }
+	  uint8_t j = 0;
+	  for (int8_t led = COL_BASES[i]; led >= COL_BASES[i] - (leds_on-1); led--){
+	    FX_DATA_BUFFER[led][0] = (10 + (20*j)) / (j+1);
+	    FX_DATA_BUFFER[led][1] = (100 - (10*j))/(j+1);
+	    FX_DATA_BUFFER[led][2] = 0;
+	    j++;
+	  }
+	}
+	lastFxRun = getTicks();
+      }
     }
-    writeData(*ACTIVE_DATA_BUFFER, PIXEL_NUMBER);
+      break;
+    }
+
     if(getButtonPressed()){
       press = (press+1)%4;
       clearButtonPress();
       clearData(PIXEL_NUMBER);
+      if (press > 1) {
+	resetBuffer(FX_DATA_BUFFER);
+	lastFxRun = 0; //Reset FX delay count
+      }
     }
+    
+    writeData(*ACTIVE_DATA_BUFFER, PIXEL_NUMBER);
   }
+}
+
+uint32_t l_rand() {
+  /* feedback polynomial: x^32 + x^30 + x^29 + x^27 + x^20 + x^15 + x^10 + x^5 + 1 */
+  LFSR_BIT = ((LFSR >> 0) ^ (LFSR >> 2) ^ (LFSR >> 3) ^ (LFSR >> 5) ^ (LFSR >> 12) ^ (LFSR >> 17) ^ (LFSR >> 22) ^ (LFSR >> 27)) & 1u;
+  return LFSR = (LFSR >> 1) | (((uint32_t)LFSR_BIT) << 31);
+}
+
+void resetBuffer(uint8_t buffer[PIXEL_NUMBER][3]){
+  memset(buffer, 0, 3*PIXEL_NUMBER); //Clear data buffer
 }
